5eee85836f8dc6b3eb613a0bed72a4ff27241aa2
Angelo Fallaria <ba.fallaria@gmail.com>
2021-11-10 08:38:44 +0800

    Remove prints in traverse_directory_tree

--- traverse_directory_tree.py
+++ traverse_directory_tree.py
@@ -4,12 +4,6 @@ import sys
 
 PROGRAM_NAME = "namcap"
 
-RED = "\033[91m"
-GREEN = "\033[32m"
-RESET = "\033[0m"
-BOLD = "\033[01m"
-DARKGRAY = "\033[37m"
-UNDERLINE = "\033[04m"
 
 def main():
     has_error = 0
@@ -20,14 +14,11 @@ def main():
             pkgbuild_dir = os.path.join(present_dir, 'PKGBUILD')
             command = f"{PROGRAM_NAME} {pkgbuild_dir}"
 
-            print(f"Checking {DARKGRAY}{pkgbuild_dir}{RESET} ...")
             result = os.system(command)
 
             # If there's an error, the checker returns a non-zero value
             if result > 0:
                 has_error = 1
-
-            print()
 
     return has_error
 
